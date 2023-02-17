3fb37d63954a7663095aa834216a959e655a26e8
huyiteng <yitengh2@illinois.edu>
2023-02-17 03:28:09 -0600

    complete CenterItem function
    container can now being automatically centered on the station

--- Assets/Scripts/Project/Container/Station.cs
+++ Assets/Scripts/Project/Container/Station.cs
@@ -13,6 +13,11 @@ public class Station : Ab_Container
     
     protected GameObject _item;  
 
+    private MeshRenderer meshRenderer;
+
+    // [SerializeField]
+    // public GameObject ObjectOnTheStation; for test only
+    
     [Tooltip("should be container or food only")]
     [SerializeField]
     public GameObject Item
@@ -29,7 +34,11 @@ public class Station : Ab_Container
     // Start is called before the first frame update
     void Start()
     {
-        
+        meshRenderer = gameObject.transform.GetComponent<MeshRenderer> ();
+
+        // string testobjname = "CubeProp";
+        // ObjectOnTheStation = GameObject.Find(testobjname);
+        // PlaceItemIntoContaier(ObjectOnTheStation);
     }
 
     // Update is called once per frame
@@ -41,18 +50,22 @@ public class Station : Ab_Container
     public override bool PlaceItemIntoContaier(GameObject gameObject) {
         Container container = this.getContainer();
         if (!this.IsEmpty() && container == null) {             // non-container occupied
+            //Debug.Log("1");
             return false;
         }
         else if (this.IsEmpty()) {                              // put directly if empty
             _item = gameObject;
+            CenterItem(gameObject);
+            //Debug.Log("2");
             return true;
         }
-        return  container.PlaceItemIntoContaier(gameObject);    // put into container 
- 
+        //Debug.Log("3");
+        return container.PlaceItemIntoContaier(gameObject);    // put into container 
     }
 
     /* either food or container, pick food from container processeded by container on it */
     public override void OnPickup(GameObject gameObject) { 
+        gameObject.transform.SetParent (null); // nullify parent relation
         _item = null;
         return;
     }
@@ -62,7 +75,25 @@ public class Station : Ab_Container
     }
 
     public Container getContainer() {
+        if (_item == null) {
+            return null;
+        }
         return _item.GetComponent<Container>();
+
+    }
+
+    private void CenterItem(GameObject go) {
+        GameObject gameObject = this.gameObject;
+        //getting externs bond
+        Vector3 goBonds = go.GetComponent<MeshRenderer>().bounds.extents;
+ 
+         //Setting the positon of the object
+        go.transform.position = new Vector3 (gameObject.transform.position.x, meshRenderer.bounds.extents.y + goBonds.y + gameObject.transform.position.y, 0);
+        go.transform.SetParent (gameObject.transform);
+
+        //should not be collided 
+        go.GetComponent<Rigidbody> ().isKinematic = true;
+        return;
     }
 
 }
