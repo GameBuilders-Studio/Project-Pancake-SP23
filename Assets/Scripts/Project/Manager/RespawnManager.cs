0925bc6b6dd0d5fbc85211c5ad1b5ac743d19d08
AashishSubramanian26 <aashish.subramanian26@gmail.com>
2023-03-29 02:06:07 -0500

    Implemented Respawning Pot

--- Assets/Scripts/Project/Manager/RespawnManager.cs
+++ Assets/Scripts/Project/Manager/RespawnManager.cs
@@ -6,19 +6,46 @@ public class RespawnManager : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
+    [SerializeField] private Transform pot;
+
+    private Vector3 originalPos; //This is to store the original position 
+
+    void Awake()
+    {
+        //if(gameObject.tag == "Pot"){
+            originalPos = pot.transform.position;
+            Debug.Log(originalPos);
+        //}
+    }
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            //player.transform.position = respawnPoint.transform.position;
             StartCoroutine(RespawnTime());
+        }
+
+        if(other.tag == "Pot")
+        {
+            //LET GO FROM PLAYER
+            /*
+            //if() if original position is being taken, check if space next to is open, if so then place object there
+            IDEAS: use vector3 distance seeing is the distance between the pots is ~0, is so, move the point to the next position
+            {
+
+            }
+            */
+            pot.transform.position = originalPos;
         }
     }
 
-    IEnumerator RespawnTime()
+    //Used as a timer so the Player must wait 5 seconds to respawn
+    IEnumerator RespawnTime() 
     {
-        yield return new WaitForSeconds(5); //wait 5 seconds to respawn the character
-        Debug.Log("Done"); 
+        yield return new WaitForSeconds(1); //wait 5 seconds to respawn the character
+        Debug.Log("Player Respawned"); 
         player.transform.position = respawnPoint.transform.position; //Set them to the respawnPoint
+        pot.transform.position = originalPos;
     }
 }
 
