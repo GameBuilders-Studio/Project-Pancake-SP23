ad55dd93a1a9777a37990a8088b9c1164ab58618
AashishSubramanian26 <aashish.subramanian26@gmail.com>
2023-03-29 16:51:01 -0500

    Working on shifting pot respawn

--- Assets/Scripts/Project/Manager/RespawnManager.cs
+++ Assets/Scripts/Project/Manager/RespawnManager.cs
@@ -7,30 +7,38 @@ public class RespawnManager : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
     [SerializeField] private Transform pot;
+    [SerializeField] private GameObject[] pots; //Make an array storing all the pots that are in the scene
 
     private Vector3 originalPos; //This is to store the original position 
 
     void Awake()
     {
-        //if(gameObject.tag == "Pot"){
-            originalPos = pot.transform.position;
-            Debug.Log(originalPos);
-        //}
+        //Store all the pots with the tag "Pot" inside the array pots
+        pots = GameObject.FindGameObjectsWithTag("Pot");
+
+        //make an array that stores all the original positions of all the pots that are in the scene
+        originalPos = pot.transform.position;
+        Debug.Log(originalPos);
     }
+
+    //When the player or pot hits the death trigger, this function will be used
     void OnTriggerEnter(Collider other)
     {
+        //If the player hits the death trigger, 
         if(other.tag == "Player")
         {
-            //player.transform.position = respawnPoint.transform.position;
             StartCoroutine(RespawnTime());
         }
 
+        //If the pot hits the death trigger
         if(other.tag == "Pot")
         {
-            //LET GO FROM PLAYER
+            //The pot needs to be let go from the player
+
             /*
             //if() if original position is being taken, check if space next to is open, if so then place object there
-            IDEAS: use vector3 distance seeing is the distance between the pots is ~0, is so, move the point to the next position
+            IDEAS: use vector3 distance seeing is the distance between the pots is ~0, 
+            this means that another pot is in another pots original postion is so, move the point to the next position using Vector3
             {
 
             }
