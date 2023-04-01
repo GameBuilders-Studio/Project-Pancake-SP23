4164a722f15f7dd22ef9428c0518b53cf326852f
CharlieDreemur <zhangwj.charlie@gmail.com>
2023-04-01 17:57:45 -0500

    scene change and implement level selection scene

---
+++ Assets/Scripts/Project/UI/LevelSelectionController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionController : MonoBehaviour
+{
+    [SerializeField] 
+    private List<SceneReference> _scenes;
+    [SerializeField] 
+    private GameObject _buttonPrefab;
+    [SerializeField]
+    private Transform _buttonParent;
+    private void Awake() {
+        int index = 1;
+        foreach (var scene in _scenes)
+        {
+            var button = Instantiate(_buttonPrefab, _buttonParent);
+            button.GetComponentInChildren<Text>().text = index.ToString();
+            button.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(scene));
+            index++;
+        }
+    }
+}
