f54863c21b3a55b43c62798365b04702c89356d9
AashishSubramanian26 <aashish.subramanian26@gmail.com>
2023-04-05 03:54:27 -0500

    Update RespawnManager.cs

--- Assets/Scripts/Project/Manager/RespawnManager.cs
+++ Assets/Scripts/Project/Manager/RespawnManager.cs
@@ -7,22 +7,22 @@ public class RespawnManager : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
     [SerializeField] private Transform pot;
-    private Stove[] stoveInScene; //Make an array storing all the pots that are in the scene
+    [SerializeField] private GameObject[] stovesInScene; //Make an array storing all the stoves that are in the scene
     
-    private Vector3 originalPos; //This is to store the original position 
+    private Vector3 originalPos; //This is to store the original position //REMOVE
 
     //IMPLEMENTATION:
-    //Create an array of the stoves that are in the scene
+    //Create an array of the stoves that are in the scene - DONE
     //When a pot falls down, we will go through the array of stoves and see if something is on top of it
     //If a stove is being occupied, we skip until the next one. If the stove is empty, we store the fallen pot 
     //on the empty stove 
 
     void Awake()
     {
-        //stoveInScene = FindGameObjectsWithTag("Stove");
-        stoveInScene = GameObject.FindObjectsOfType<Stove>();
-
-        originalPos = pot.transform.position;
+        stovesInScene = GameObject.FindGameObjectsWithTag("Stove");
+        Debug.Log(stovesInScene.Length);
+;
+        originalPos = pot.transform.position; //REMOVE
     }
 
     //When the player or pot hits the death trigger, this function will be used
@@ -40,18 +40,18 @@ public class RespawnManager : MonoBehaviour
         //If the pot hits the death triggere
         if(other.tag == "Pot")
         {
-
-            /*
-            //use GameObject.FindGameObjectWithTag("Your_Tag_Here").transform.position; if this is equal to current position of pot that needs to respawns,
-            move the pot to new position and check again is position is avaiable
-            //if() if original position is being taken, check if space next to is open, if so then place object there
-            IDEAS: use vector3 distance seeing is the distance between the pots is ~0, 
-            this means that another pot is in another pots original postion is so, move the point to the next position using Vector3
+            //We will iterate through each of the stoves in the scene
+            foreach (GameObject stoves in stovesInScene)
             {
+                //if a stove is not occupied, put the pot on that empty stove
+                if(stoves)
+                {
+                    //Call PlaceItem from Station.cs -> place pot on stove
 
+                }
             }
-            */
-            pot.transform.position = originalPos;
+            
+            pot.transform.position = originalPos; //REMOVE
         }
     }
 
@@ -61,6 +61,5 @@ public class RespawnManager : MonoBehaviour
         yield return new WaitForSeconds(1); //wait 5 seconds to respawn the character
         Debug.Log("Player Respawned"); 
         player.transform.position = respawnPoint.transform.position; //Set them to the respawnPoint
-        //pot.transform.position = originalPos;
     }
 }
