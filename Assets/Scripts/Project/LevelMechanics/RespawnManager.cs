940e0602abd263b1ed28930d3694f3e3630fede6
AashishSubramanian26 <aashish.subramanian26@gmail.com>
2023-04-09 19:41:02 -0500

    Pot Respawning Works

--- Assets/Scripts/Project/LevelMechanics/RespawnManager.cs
+++ Assets/Scripts/Project/LevelMechanics/RespawnManager.cs
@@ -7,7 +7,8 @@ public class RespawnManager : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
     [SerializeField] private List<Station> stovesInScene = new List<Station>(); //Make an array storing all the stoves that are in the scene
-     
+
+    private Station empty;
     private Vector3 originalPos; //This is to store the original position //REMOVE
 
     //IMPLEMENTATION:
@@ -19,23 +20,25 @@ public class RespawnManager : MonoBehaviour
     void Awake()
     {
         Object[] stoves = GameObject.FindObjectsOfType(typeof(Stove));
-        Debug.Log(stoves.Length);
-        foreach(Stove obj in stoves) {
-            Station station = obj.GetComponent<Station>(); 
-            if(station != null) {
+        foreach (Stove obj in stoves)
+        {
+            Station station = obj.GetComponent<Station>();
+            if (station == null)
+            {
                 Debug.LogError("Every Stove should have Station component!");
             }
+            stovesInScene.Add(station);
         }
 
-        Debug.Log(stovesInScene.Count);
-        Debug.Log(stovesInScene);
+        Debug.Log("STOVES IN SCENE COUNT: " + stovesInScene.Count);
     }
 
     //When the player or pot hits the death trigger, this function will be used
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Object entered triggerbox");
         //If the player hits the death trigger, 
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
             StartCoroutine(RespawnTime());
 
@@ -44,30 +47,34 @@ public class RespawnManager : MonoBehaviour
         }
 
         //If the pot hits the death triggere
-        if(other.tag == "Pot")
+        if (other.tag == "Pot")
         {
+            Debug.Log("Pot entered triggerbox");
+            Carryable carryable = other.GetComponent<Carryable>();
+            if (carryable == null)
+            {
+                Debug.LogError("Objects with Pot tag should have Carryable component");
+            }
+            Debug.Log(stovesInScene.Count);
             //We will iterate through each of the stoves in the scene
-            foreach (Stove stoves in stovesInScene)
+            foreach (Station stoves in stovesInScene)
             {
                 //if a stove is not occupied, put the pot on that empty stove
-                //if(stoves)
-                //{
+                if (stoves.PlacedItem == null) //EMPTY
+                {
                     // Check if station has a placed item using the public property 
+                    stoves.PlaceItem(carryable);
+                }
 
-                    //Call PlaceItem from Station.cs -> place pot on stove 
-
-                    // 
-
-                //}
             }
         }
     }
 
     //Used as a timer so the Player must wait 5 seconds to respawn
-    IEnumerator RespawnTime() 
+    IEnumerator RespawnTime()
     {
         yield return new WaitForSeconds(1); //wait 5 seconds to respawn the character
-        Debug.Log("Player Respawned"); 
+        Debug.Log("Player Respawned");
         player.transform.position = respawnPoint.transform.position; //Set them to the respawnPoint
     }
 }
