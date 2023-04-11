01ccff59cf1c8a327852a3c6b6f872ce41c09149
AashishSubramanian26 <aashish.subramanian26@gmail.com>
2023-04-11 01:59:27 -0500

    Respawn Complete :)

--- Assets/Scripts/Project/LevelMechanics/RespawnManager.cs
+++ Assets/Scripts/Project/LevelMechanics/RespawnManager.cs
@@ -6,10 +6,10 @@ public class RespawnManager : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
-    [SerializeField] private Transform PotRespawnPoint;
+    [SerializeField] private Transform PotRespawnPoint; 
     [SerializeField] private PlayerInteraction playerHands; //used for the player to let go of the object if currently holding.
-    [SerializeField] private List<Station> stovesInScene = new List<Station>(); //Make an array storing all the stoves that are in the scene
-
+    private List<Station> stovesInScene = new List<Station>(); //Make an array storing all the stoves that are in the scene
+    private List<Station> tablesInScene = new List<Station>(); //Used to place when we have to put our extra pots and pans on table
     //IMPLEMENTATION:
     //Create an array of the stoves that are in the scene - DONE
     //When a pot falls down, we will go through the array of stoves and see if something is on top of it
@@ -18,6 +18,7 @@ public class RespawnManager : MonoBehaviour
 
     void Awake()
     {
+        //Helps initialize the amount of stoves that are in the scene
         Object[] stoves = GameObject.FindObjectsOfType(typeof(Stove));
         foreach (Stove obj in stoves)
         {
@@ -29,7 +30,16 @@ public class RespawnManager : MonoBehaviour
             stovesInScene.Add(station);
         }
 
+        //make a list of all the Tables in the scene
+        Object[] tables = GameObject.FindObjectsOfType(typeof(Table));
+        foreach(Table obj1 in tables)
+        {
+            Station station1 = obj1.GetComponent<Station>();
+            tablesInScene.Add(station1);
+        }
+
         Debug.Log("STOVES IN SCENE COUNT: " + stovesInScene.Count);
+        Debug.Log("TABLES IN SCENE COUNT: " + tablesInScene.Count);
     }
 
     //When the player or pot hits the death trigger, this function will be used
@@ -67,20 +77,30 @@ public class RespawnManager : MonoBehaviour
                     return;
                 }
             }
-            //openTables(carryable);
-            //Moves the pot to position if all stoves are taken
-            carryable.transform.position = PotRespawnPoint.transform.position;
+            //When there all the stoves are occupied and the object can't go back onto the stove
+            openTables(carryable);
         }
     }
 
-    // private void openTables(Carryable carryable){
-
-    // }
+     private void openTables(Carryable carryable){
+        foreach (Station tables in tablesInScene)
+            {
+                //if a stove is not occupied, put the pot on that empty stove
+                if (tables.PlacedItem == null) //EMPTY
+                {
+                    // Check if station has a placed item using the public property 
+                    Debug.Log("Placed on table");
+                    tables.PlaceItem(carryable);
+                    return;
+                }
+            }
+        carryable.transform.position = PotRespawnPoint.transform.position; //This will probably never happen
+     }
 
     //Used as a timer so the Player must wait 5 seconds to respawn
     IEnumerator RespawnTime()
     {
-        yield return new WaitForSeconds(1); //wait 5 seconds to respawn the character
+        yield return new WaitForSeconds(5); //wait 5 seconds to respawn the character
         Debug.Log("Player Respawned");
         player.transform.position = respawnPoint.transform.position; //Set them to the respawnPoint
     }
