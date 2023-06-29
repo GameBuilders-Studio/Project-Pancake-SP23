03b4e0fdacfaaad590ceba229d2971166f16ce44
Jacky Park <jackypark9852@gmail.com>
2023-06-29 05:03:26 -0400

    Add item pickup/drop animation logic

--- Assets/Scripts/Project/Player/PlayerAnimation.cs
+++ Assets/Scripts/Project/Player/PlayerAnimation.cs
@@ -5,17 +5,26 @@ public class PlayerAnimation : MonoBehaviour
     [Tooltip("The speed at which the player will transition from idle to running animation.")]
     [SerializeField] float _runningSpeedThreshold = 0.1f;
     Animator _animator;
+    PlayerInteraction _playerInteraction;
     Vector3 _previousPosition;
     bool _isRunning = false;
+    bool _isHolding = false;
 
     private void Awake() {
         _animator = GetComponentInChildren<Animator>(); // Animator component is on child fbx object
+        _playerInteraction = GetComponent<PlayerInteraction>();
+    }
+
+    private void Start() {
+        _playerInteraction.PickUpItemEvent += PlayPickUp;
+        _playerInteraction.PlaceItemEvent += PlayDrop;
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckIfRunning();
+        CheckIfHolding();
     }
 
     private void CheckIfRunning() {
@@ -27,5 +36,20 @@ public class PlayerAnimation : MonoBehaviour
             _isRunning = speed > _runningSpeedThreshold; 
             _animator.SetBool("IsRunning", _isRunning); // Update the state once per change
         }
+    }
+
+    private void CheckIfHolding() {
+        if(_playerInteraction.IsCarrying != _isHolding) { // Check if holding state has changed
+            _isHolding = _playerInteraction.IsCarrying;
+            _animator.SetBool("IsHolding", _isHolding); // Update the state once per change
+        }
+    }
+
+    private void PlayPickUp() {
+        _animator.SetTrigger("PickUp");
+    }
+
+    private void PlayDrop() {
+        _animator.SetTrigger("Drop");
     }
 }
