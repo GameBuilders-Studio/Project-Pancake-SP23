8347c76e3b4665a659f2992a1d03171103fb5cf0
Jacky Park <jackypark9852@gmail.com>
2023-08-25 09:34:33 -0500

    Implement stars filling

--- Assets/Scripts/Project/UI/LevelEndData.cs
+++ Assets/Scripts/Project/UI/LevelEndData.cs
@@ -12,9 +12,14 @@ public class LevelEndData : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _scoreText; 
 
+    [SerializeField]
+    private StarBar _starBar;
+
     private void Start()
     {
         _levelNameText.text = DataCapsule.Instance.lastLevel;
         _scoreText.text = DataCapsule.Instance.score.ToString();
+        _starBar.SetTargetFraction((float) DataCapsule.Instance.score / DataCapsule.Instance.scoreBarMax);
+        _starBar.PlayStarAnimation();
     }
 }
